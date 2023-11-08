208b14730504bfc1daa01461f67787e76e47a804
jkimdev <jkim.dev@icloud.com>
2023-11-09 01:05:25 +0900

    font system

--- SsdamApp/Sources/Extension/FontExtension.swift
+++ SsdamApp/Sources/Extension/FontExtension.swift
@@ -6,4 +6,23 @@
 //  Copyright © 2023 com.dduckdori. All rights reserved.
 //
 
-import Foundation
+import SwiftUI
+
+extension Font {
+    // headline
+    static let headline1: Font = .custom("Pretendard-SemiBold", size: 26)
+    static let headline2: Font = .custom("Pretendard-SemiBold", size: 20)
+    static let headline3: Font = .custom("Pretendard-Medium", size: 18)
+    static let headline4: Font = .custom("Pretendard-Medium", size: 16)
+    
+    // button
+    static let button1: Font = .custom("Pretendard-SemiBold", size: 18)
+    static let button2: Font = .custom("Pretendard-SemiBold", size: 16)
+    
+    // body
+    static let body1: Font = .custom("Pretendard-Regular", size: 22)
+    static let body2: Font = .custom("Pretendard-Regular", size: 14)
+    
+    // caption
+    static let caption: Font = .custom("Pretendard-Regular", size: 16)
+}
