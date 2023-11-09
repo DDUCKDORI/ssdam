b15b15a322447951e6bb2f0e2487d58c84dbd7bb
jkimdev <jkim.dev@icloud.com>
2023-11-09 14:58:24 +0900

    Color system

--- SsdamApp/Sources/ContentView.swift
+++ SsdamApp/Sources/ContentView.swift
@@ -13,6 +13,7 @@ public struct ContentView: View {
                 .padding()
             Text("Hello, World!")
                 .font(.custom("Pretendard-SemiBold", size: 50))
+                .foregroundStyle(Colors.gray10)
                 .padding()
         }
         .onAppear {
