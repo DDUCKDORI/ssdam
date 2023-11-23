9b985c1a0c5603d0d06e9942a7e5296c5ce40e8f
jkimdev <jkim.dev@icloud.com>
2023-11-23 12:38:22 +0900

    add image resources for login view

---
+++ SsdamApp/Sources/Viewes/Login/LoginView.swift
@@ -0,0 +1,19 @@
+//
+//  LoginView.swift
+//  Ssdam
+//
+//  Created by 김재민 on 2023/11/22.
+//  Copyright © 2023 com.dduckdori. All rights reserved.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
---
+++ SsdamApp/Sources/Viewes/Login/UserTypeView.swift
@@ -0,0 +1,19 @@
+//
+//  UserTypeView.swift
+//  Ssdam
+//
+//  Created by 김재민 on 2023/11/16.
+//  Copyright © 2023 com.dduckdori. All rights reserved.
+//
+
+import SwiftUI
+
+struct UserTypeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserTypeView()
+}
