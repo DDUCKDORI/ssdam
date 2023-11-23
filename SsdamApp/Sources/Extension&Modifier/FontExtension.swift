5a4729d030d8266582d3fd82b386bc81dc3a5405
jkimdev <jkim.dev@icloud.com>
2023-11-23 12:38:51 +0900

    change font system

--- SsdamApp/Sources/Extension&Modifier/FontExtension.swift
+++ SsdamApp/Sources/Extension&Modifier/FontExtension.swift
@@ -10,19 +10,20 @@ import SwiftUI
 
 extension Font {
     // headline
-    static let pHeadline: Font = .custom(SsdamFontFamily.Pretendard.semiBold.family, size: 26)
-    static let pHeadline2: Font = .custom(SsdamFontFamily.Pretendard.semiBold.family, size: 20)
-    static let pHeadline3: Font = .custom(SsdamFontFamily.Pretendard.medium.family, size: 18)
-    static let pHeadline4: Font = .custom(SsdamFontFamily.Pretendard.medium.family, size: 16)
+    static let pHeadline: Font = .custom(SsdamFontFamily.Pretendard.semiBold.name, size: 26)
+    static let pHeadline2: Font = .custom(SsdamFontFamily.Pretendard.semiBold.name, size: 20)
+    static let pHeadline3: Font = .custom(SsdamFontFamily.Pretendard.medium.name, size: 18)
     
     // button
-    static let pButton: Font = .custom(SsdamFontFamily.Pretendard.semiBold.family, size: 18)
-    static let pButton2: Font = .custom(SsdamFontFamily.Pretendard.semiBold.family, size: 16)
-    
+    static let pButton: Font = .custom(SsdamFontFamily.Pretendard.bold.name, size: 18)
+    static let pButton2: Font = .custom(SsdamFontFamily.Pretendard.semiBold.name, size: 18)
+    static let pButton3: Font = .custom(SsdamFontFamily.Pretendard.bold.name, size: 16)
+    static let pButton4: Font = .custom(SsdamFontFamily.Pretendard.semiBold.name, size: 16)
+
     // body
-    static let pBody: Font = .custom(SsdamFontFamily.Pretendard.regular.family, size: 22)
-    static let pBody2: Font = .custom(SsdamFontFamily.Pretendard.regular.family, size: 14)
+    static let pBody: Font = .custom(SsdamFontFamily.Pretendard.regular.name, size: 16)
+    static let pBody2: Font = .custom(SsdamFontFamily.Pretendard.regular.name, size: 14)
     
     // caption
-    static let pCaption: Font = .custom(SsdamFontFamily.Pretendard.regular.family, size: 16)
+    static let pCaption: Font = .custom(SsdamFontFamily.Pretendard.medium.name, size: 16)
 }
