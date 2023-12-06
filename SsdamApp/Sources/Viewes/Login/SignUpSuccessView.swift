0e20a70c06428fd5d1c389e54d4baed46d34a4ea
jkimdev <jkim.dev@icloud.com>
2023-12-06 17:51:36 +0900

    modify SignUpView

--- SsdamApp/Sources/Viewes/Login/SignUpSuccessView.swift
+++ SsdamApp/Sources/Viewes/Login/SignUpSuccessView.swift
@@ -7,50 +7,99 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
+
+struct SignUpSuccessReducer: Reducer {
+    
+    struct State: Equatable {
+        var titles: [String] = [
+            "",
+            "이제부터 가족과 질문에\n대한 생각을 기록할 수 있어요",
+            "답변을 완료하면 매일 아침\n9시에 새로운 질문이 도착해요",
+            "소중한 우리 가족의 기록을 지금 남겨보세요 :)"
+        ]
+        var images: [ImageResource] = [.guide1, .guide2, .guide3, .guide4]
+        var currentPage: Int = 0
+        var nickname: String
+        var welcomeMessage: AttributedString {
+            var text: AttributedString = AttributedString(stringLiteral: "\(nickname)님 환영합니다")
+            let nicknameRange = text.range(of: "\(nickname)")!
+            text[nicknameRange].font = .pHeadline2
+            text[nicknameRange].foregroundColor = Color(.mint50)
+            
+            return text
+        }
+    }
+    
+    enum Action: Equatable {
+        case pageChanged(Int)
+        case tapNextButton
+    }
+    
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case let .pageChanged(page):
+                state.currentPage = page
+                return .none
+            case .tapNextButton:
+                state.currentPage += 1
+                return .none
+            }
+        }
+    }
+}
 
 struct SignUpSuccessView: View {
+    @EnvironmentObject var screenRouter: ScreenRouter
+    let store: StoreOf<SignUpSuccessReducer>
     var body: some View {
-        ZStack {
-            Color(.yellow50)
-                .ignoresSafeArea()
-            VStack {
-                Image(.characterSuccess)
-                    .resizable()
-                    .frame(height: 339)
-                    .padding(.bottom, 23)
-                    .padding(.horizontal, 36)
-                
-                var welcomeMessage: AttributedString {
-                    var text: AttributedString = "shinhye님\n환영합니다"
-                    let nicknameRange = text.range(of: "shinhye")!
-                    let messageRange = text.range(of: "님\n환영합니다")!
-                    
-                    text[nicknameRange].font = .pHeadline
-                    text[messageRange].font = .pHeadline
-                    text[nicknameRange].foregroundColor = Color(.mint50)
-                    
-                    return text
+        WithViewStore(self.store, observe:  { $0 } ) { viewStore in
+            ZStack {
+                Color(.yellow50)
+                    .ignoresSafeArea()
+                PageViewController(pages: viewStore.images.map { Image($0) }, currentPage: viewStore.binding(get: \.currentPage, send: { value in
+                        .pageChanged(value)
+                }))
+                Group {
+                    if viewStore.currentPage == 0 {
+                        Text(viewStore.welcomeMessage)
+                    }
+                    else {
+                        Text(viewStore.titles[viewStore.currentPage])
+                            .font(.pHeadline2)
+                    }
                 }
-                Text(welcomeMessage)
-                    .padding(.bottom, 106)
+                .offset(y: -228)
+                
+                PageControl(numberOfPages: viewStore.images.count, currentPage: viewStore.binding(get: \.currentPage, send: { value in
+                    .pageChanged(value) }))
+                .offset(y: 210)
                 
                 Button(action: {
-                    
+                    if viewStore.currentPage < 3 { 
+                        store.send(.tapNextButton)
+                        return
+                    }
+                    screenRouter.change(.home)
                 }, label: {
-                    Text("다음")
+                    Text(viewStore.currentPage < 3 ? "다음" : "시작하기")
                         .font(.pButton)
-                        .foregroundStyle(Color(.white))
+                        .foregroundStyle(.white)
                         .padding(.vertical, 16)
                         .frame(maxWidth: .infinity)
                         .background(Color(.mint50))
                         .clipShape(RoundedRectangle(cornerRadius: 100))
                         .padding(.horizontal, 80)
                 })
+                .offset(y: 280)
             }
         }
     }
 }
 
 #Preview {
-    SignUpSuccessView()
+    SignUpSuccessView(store: .init(initialState: SignUpSuccessReducer.State(nickname: "jimmy"), reducer: {
+        SignUpSuccessReducer()
+    }))
 }
