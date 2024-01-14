c10bfbbae615164feeb32e77afd9672d4b334e5b
jkimdev <jkim.dev@icloud.com>
2024-01-14 23:46:45 +0900

    answer cardview with reducer

--- SsdamApp/Sources/Viewes/Components/HomeAnswerCard.swift
+++ SsdamApp/Sources/Viewes/Components/HomeAnswerCard.swift
@@ -7,46 +7,77 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
+
+struct HomeAnswerCardReducer: Reducer {
+    struct State: Equatable {
+        var isExpanded: Bool = false
+        var payloads: [FetchAnswerPayload] = []
+    }
+    
+    enum Action: Equatable {
+        case expand
+        case editAnswer
+    }
+    
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case .expand:
+                state.isExpanded.toggle()
+                return .none
+            case .editAnswer:
+                return .none
+            }
+        }
+    }
+}
 
 struct HomeAnswerCard: View {
-    let payload: FetchAnswerPayload
-    @State var isExpanded: Bool = false
+    let store: StoreOf<HomeAnswerCardReducer>
     var body: some View {
-        VStack(spacing: 0) {
-            HStack {
-                Text("\(payload.memberId)의 답변")
-                    .font(.pButton4)
-                Spacer()
-                HStack(spacing: 10) {
-                    Text(!payload.answer.isEmpty ? "2023.12.10" : "")
-                        .font(.pBody2)
-                        .foregroundStyle(Color(.gray60))
-                    Image(!payload.answer.isEmpty ? .checkmarkCircleMint : .checkmarkCircle)
-                }
-            }
-            .padding(.horizontal, 24)
-            .padding(.vertical, 17)
-            .background(!payload.answer.isEmpty ? Color(.mint20) : Color(.gray10))
-            .overlay(RoundedCorner(radius: 10, corners: isExpanded ? [.topLeft, .topRight] : .allCorners)
-                .stroke(!payload.answer.isEmpty ? Color(.mint50) : Color(.gray20), lineWidth: 2)
-            )
-
-            .onTapGesture {
-                if !payload.answer.isEmpty {
-                    self.isExpanded.toggle()
-                }
-            }
-            if isExpanded {
-                Text(payload.answer)
-                    .font(.pBody)
-                    .padding(.vertical, 48)
-                    .padding(.horizontal, 52)
-                    .frame(maxWidth: .infinity)
-                    .background(Color.white)
-                    .overlay(RoundedCorner(radius: 10, corners: [.bottomLeft, .bottomRight])
-                        .stroke(Color(.mint50), lineWidth: 2)
+        WithViewStore(self.store, observe: { $0 }) { viewStore in
+            ForEach(viewStore.payloads, id: \.self) { payload in
+                VStack(spacing: 0) {
+                    HStack {
+                        Text("\(payload.memberId)의 답변")
+                            .font(.pButton4)
+                        Spacer()
+                        HStack(spacing: 10) {
+                            Text(!payload.answer.isEmpty ? "2023.12.10" : "")
+                                .font(.pBody2)
+                                .foregroundStyle(Color(.gray60))
+                            Image(!payload.answer.isEmpty ? .checkmarkCircleMint : .checkmarkCircle)
+                        }
+                    }
+                    .padding(.horizontal, 24)
+                    .padding(.vertical, 17)
+                    .background(!payload.answer.isEmpty ? Color(.mint20) : Color(.gray10))
+                    .overlay(RoundedCorner(radius: 10, corners: viewStore.isExpanded ? [.topLeft, .topRight] : .allCorners)
+                        .stroke(!payload.answer.isEmpty ? Color(.mint50) : Color(.gray20), lineWidth: 2)
                     )
-                    .transition(.move(edge: .top))
+                    
+                    .onTapGesture {
+                        if !payload.answer.isEmpty {
+                            viewStore.send(.expand)
+                        }
+                    }
+                    if viewStore.isExpanded {
+                        Text(payload.answer)
+                            .font(.pBody)
+                            .padding(.vertical, 48)
+                            .padding(.horizontal, 52)
+                            .frame(maxWidth: .infinity)
+                            .background(Color.white)
+                            .overlay(RoundedCorner(radius: 10, corners: [.bottomLeft, .bottomRight])
+                                .stroke(Color(.mint50), lineWidth: 2)
+                            )
+                            .transition(.move(edge: .top))
+                            .onTapGesture {
+                                viewStore.send(.editAnswer)
+                            }
+                    }
+                }
             }
         }
     }
--- SsdamApp/Sources/Viewes/Tabs/Home/AnswerListView.swift
+++ SsdamApp/Sources/Viewes/Tabs/Home/AnswerListView.swift
@@ -10,12 +10,17 @@ import SwiftUI
 import ComposableArchitecture
 import Domain
 import Utils
+import Networking
 
 struct AnswerListReducer: Reducer {
+    @Dependency(\.screenRouter) var screenRouter
     @Dependency(\.mainUseCase) var mainUseCase
     struct State: Equatable {
-        var answerPayloads: [FetchAnswerPayload] = []
         var questionPayload: QuestionPayload = .init()
+        var writeState = WriteReducer.State()
+        var cardState = HomeAnswerCardReducer.State()
+        var answerPayload: RequestAnswerPayload = .init()
+        @PresentationState var isPresented: PresentationState<Bool>?
     }
     
     enum Action: Equatable {
@@ -23,25 +28,22 @@ struct AnswerListReducer: Reducer {
         case answerReponse(TaskResult<[FetchAnswerEntity]>)
         case fetchQuestion(String)
         case makeQuestionPayload(TaskResult<FetchQuestionEntity>)
+        case writeAction(WriteReducer.Action)
+        case requestAnswer(PostAnswerBody)
+        case requestAnswerResponse(TaskResult<RequestAnswerEntity>)
+        case presentSheet(PresentationAction<Bool>)
+        case cardAction(HomeAnswerCardReducer.Action)
     }
     
     var body: some ReducerOf<Self> {
+        Scope(state: \.writeState, action: /Action.writeAction) {
+            WriteReducer()
+        }
+        Scope(state: \.cardState, action: /Action.cardAction) {
+            HomeAnswerCardReducer()
+        }
         Reduce { state, action in
             switch action {
-            case let .fetchAnswer(id):
-                return .run { send in
-                    let result = await TaskResult {
-                        let data = await mainUseCase.fetchAllAnswers(id: id)
-                        return data
-                    }
-                    await send(.answerReponse(result))
-                }
-            case let .answerReponse(.success(entity)):
-                state.answerPayloads = entity.map { FetchAnswerPayload($0) }
-                return .none
-            case let .answerReponse(.failure(error)):
-                print(error.localizedDescription)
-                return .none
             case let .fetchQuestion(id):
                 return .run { send in
                     let result = await TaskResult {
@@ -55,6 +57,55 @@ struct AnswerListReducer: Reducer {
                 return .send(.fetchAnswer("\(state.questionPayload.categoryId)_\(state.questionPayload.questionId)_\(Const.inviteCd)"))
             case let .makeQuestionPayload(.failure(error)):
                 print(error.localizedDescription)
+                return .none
+            case let .fetchAnswer(id):
+                return .run { send in
+                    let result = await TaskResult {
+                        let data = await mainUseCase.fetchAllAnswers(id: id)
+                        return data
+                    }
+                    await send(.answerReponse(result))
+                }
+            case let .answerReponse(.success(entity)):
+                state.cardState.payloads = entity.map { FetchAnswerPayload($0) }
+                return .none
+            case let .answerReponse(.failure(error)):
+                print(error.localizedDescription)
+                return .none
+            case .writeAction(.answerButtonTapped):
+                let body = PostAnswerBody(
+                    cateId: state.questionPayload.categoryId,
+                    qustId: state.questionPayload.questionId,
+                    memId: Const.memId,
+                    inviteCd: Const.inviteCd,
+                    ansCn: state.writeState.text)
+                return .send(.requestAnswer(body))
+            case let .requestAnswer(body):
+                return .run { send in
+                    let result = await TaskResult {
+                        let data = await mainUseCase.modifyAnswer(request: body)
+                        return data
+                    }
+                    await send(.requestAnswerResponse(result))
+                }
+            case let .requestAnswerResponse(.success(data)):
+                state.answerPayload = RequestAnswerPayload(data)
+                screenRouter.dismiss()
+                return .none
+            case let .requestAnswerResponse(.failure(error)):
+                print(error.localizedDescription)
+                return .none
+            case .cardAction(.editAnswer):
+                return .send(.presentSheet(.presented(true)))
+            case .presentSheet(.presented(true)):
+                state.writeState.question = state.questionPayload.quesContent
+                state.writeState.text = state.questionPayload.ansContent
+                state.isPresented = PresentationState(wrappedValue: true)
+                return .none
+            case .presentSheet(.dismiss):
+                state.isPresented = nil
+                return .none
+            default:
                 return .none
             }
         }
@@ -80,9 +131,7 @@ struct AnswerListView: View {
                             .padding(.bottom, 26)
                         
                         VStack(spacing: 16) {
-                            ForEach(viewStore.answerPayloads, id: \.self) { answer in
-                                HomeAnswerCard(payload: answer)
-                            }
+                                HomeAnswerCard(store: self.store.scope(state: \.cardState, action: AnswerListReducer.Action.cardAction))
                         }
                         .padding(.horizontal, 30)
                     }
@@ -90,6 +139,9 @@ struct AnswerListView: View {
                 .padding(.top, 142)
             }
             .ignoresSafeArea()
+            .fullScreenCover(store: self.store.scope(state: \.$isPresented, action: AnswerListReducer.Action.presentSheet), onDismiss: { viewStore.send(.presentSheet(.dismiss)) }) { store in
+                WriteView(store: self.store.scope(state: \.writeState, action: AnswerListReducer.Action.writeAction))
+            }
             .onAppear {
                 viewStore.send(.fetchQuestion("\(Const.inviteCd)_\(Const.memId)"))
             }
