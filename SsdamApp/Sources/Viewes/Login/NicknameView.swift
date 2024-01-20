99bda7b0f0a3e61cfae22f7a4cb2f32926261311
jkimdev <jkim.dev@icloud.com>
2024-01-20 20:25:20 +0900

    hide keyboard with background touch

--- SsdamApp/Sources/Viewes/Login/NicknameView.swift
+++ SsdamApp/Sources/Viewes/Login/NicknameView.swift
@@ -19,9 +19,10 @@ struct NicknameReducer: Reducer {
         var isValid: Bool = false
         var serviceAgreement: Bool = false
         var privacyAgreement: Bool = false
+        @BindingState var focusedField: Bool = false
     }
     
-    enum Action: Equatable {
+    enum Action: BindableAction, Equatable {
         case nicknameChanged(String)
         case nicknameValidation(String)
         case agreeAllToggled(Bool)
@@ -29,9 +30,11 @@ struct NicknameReducer: Reducer {
         case privacyAgreementToggled(Bool)
         case login([String: String])
         case loginResponse(TaskResult<TokenEntity>)
+        case binding(BindingAction<State>)
     }
     
     var body: some ReducerOf<Self> {
+        BindingReducer()
         Reduce { state, action in
             switch action {
             case let .nicknameChanged(newValue):
@@ -72,6 +75,8 @@ struct NicknameReducer: Reducer {
                 return .none
             case .loginResponse(.failure(_)):
                 return .none
+            case .binding:
+                return .none
             }
         }
     }
@@ -79,114 +84,124 @@ struct NicknameReducer: Reducer {
 
 struct NicknameView: View {
     let store: StoreOf<NicknameReducer>
+    @FocusState var focusedField: Bool
     
     var body: some View {
         WithViewStore(self.store, observe: { $0 }) { viewStore in
-            VStack(spacing: 16) {
-                viewStore.isValid ?
-                Text("멋진 닉네임이네요 :)")
-                    .font(.pHeadline2)
-                    .foregroundStyle(Color(.systemBlack))
-                :
-                Text("사용할 닉네임을 입력해주세요")
-                    .font(.pHeadline2)
-                    .foregroundStyle(Color(.systemBlack))
-                
-                ZStack(alignment: .trailing) {
-                    TextField("1~10자 이내 작성", text: viewStore.binding(get: \.nickname, send: { value in
-                            .nicknameChanged(value)
-                    }))
-                    .font(.pButton)
-                    .foregroundStyle(Color(.systemBlack))
-                    .padding(.vertical, 16)
-                    .multilineTextAlignment(.center)
-                    .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color(.mint50), lineWidth: 2))
-                    .onChange(of: viewStore.nickname){ _, newValue in
-                        viewStore.send(.nicknameValidation(newValue))
+            ZStack {
+                Color.white
+                VStack(spacing: 16) {
+                    viewStore.isValid ?
+                    Text("멋진 닉네임이네요 :)")
+                        .font(.pHeadline2)
+                        .foregroundStyle(Color(.systemBlack))
+                    :
+                    Text("사용할 닉네임을 입력해주세요")
+                        .font(.pHeadline2)
+                        .foregroundStyle(Color(.systemBlack))
+                    
+                    ZStack(alignment: .trailing) {
+                        TextField("1~10자 이내 작성", text: viewStore.binding(get: \.nickname, send: { value in
+                                .nicknameChanged(value)
+                        }))
+                        .font(.pButton)
+                        .foregroundStyle(Color(.systemBlack))
+                        .padding(.vertical, 16)
+                        .multilineTextAlignment(.center)
+                        .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color(.mint50), lineWidth: 2))
+                        .focused($focusedField)
+                        .onChange(of: viewStore.nickname){ _, newValue in
+                            viewStore.send(.nicknameValidation(newValue))
+                            
+                        }
+                        .bind(viewStore.$focusedField, to: self.$focusedField)
                         
+                        if viewStore.isValid {
+                            Image(.checkmarkCircleMint)
+                                .padding(.vertical, 16)
+                                .padding(.horizontal, 24)
+                        }
+                    }
+                    if viewStore.nickname.count > 0, !viewStore.isValid {
+                        Text("*닉네임을 다시 입력해주세요")
+                            .font(.pCaption)
+                            .foregroundStyle(Color(.systemRed))
+                            .padding(.vertical, 12)
                     }
                     if viewStore.isValid {
-                        Image(.checkmarkCircleMint)
-                            .padding(.vertical, 16)
-                            .padding(.horizontal, 24)
-                    }
-                }
-                if viewStore.nickname.count > 0, !viewStore.isValid {
-                    Text("*닉네임을 다시 입력해주세요")
-                        .font(.pCaption)
-                        .foregroundStyle(Color(.systemRed))
-                        .padding(.vertical, 12)
-                }
-                if viewStore.isValid {
-                    HStack {
-                        Text("모두 동의합니다")
-                            .font(.pButton4)
-                            .foregroundStyle(viewStore.serviceAgreement && viewStore.privacyAgreement ? Color(.systemBlack) : Color(.gray20))
-                        Spacer()
-                        Image(viewStore.serviceAgreement && viewStore.privacyAgreement ? .checkmarkCircleMint : .checkmarkCircle )
-                    }
-                    .padding(.vertical, 16)
-                    .padding(.horizontal, 24)
-                    .background(Color(.gray10))
-                    .clipShape(RoundedRectangle(cornerRadius: 10))
-                    .onTapGesture {
-                        if !viewStore.serviceAgreement || !viewStore.privacyAgreement {
-                            viewStore.send(.agreeAllToggled(true))
-                            return
+                        HStack {
+                            Text("모두 동의합니다")
+                                .font(.pButton4)
+                                .foregroundStyle(viewStore.serviceAgreement && viewStore.privacyAgreement ? Color(.systemBlack) : Color(.gray20))
+                            Spacer()
+                            Image(viewStore.serviceAgreement && viewStore.privacyAgreement ? .checkmarkCircleMint : .checkmarkCircle )
                         }
-                        viewStore.send(.agreeAllToggled(false))
+                        .padding(.vertical, 16)
+                        .padding(.horizontal, 24)
+                        .background(Color(.gray10))
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .onTapGesture {
+                            if !viewStore.serviceAgreement || !viewStore.privacyAgreement {
+                                viewStore.send(.agreeAllToggled(true))
+                                return
+                            }
+                            viewStore.send(.agreeAllToggled(false))
+                        }
+                        
+                        HStack {
+                            Text("(필수) 서비스 이용약관")
+                                .font(.pBody)
+                                .foregroundStyle(Color(.systemBlack))
+                            Spacer()
+                            Image(viewStore.serviceAgreement ? .checkmarkCircleFill : .checkmarkCircle )
+                        }
+                        .padding(.vertical, -3)
+                        .padding(.horizontal, 24)
+                        .onTapGesture {
+                            viewStore.send(.serviceAgreementToggled(viewStore.serviceAgreement))
+                        }
+                        
+                        HStack {
+                            Text("(필수) 개인정보 처리방침")
+                                .font(.pBody)
+                                .foregroundStyle(Color(.systemBlack))
+                            Spacer()
+                            Image(viewStore.privacyAgreement ? .checkmarkCircleFill : .checkmarkCircle )
+                        }
+                        .padding(.horizontal, 24)
+                        .onTapGesture {
+                            viewStore.send(.privacyAgreementToggled(viewStore.serviceAgreement))
+                        }
+                        .padding(.bottom, 106)
                     }
                     
-                    HStack {
-                        Text("(필수) 서비스 이용약관")
-                            .font(.pBody)
-                            .foregroundStyle(Color(.systemBlack))
-                        Spacer()
-                        Image(viewStore.serviceAgreement ? .checkmarkCircleFill : .checkmarkCircle )
-                    }
-                    .padding(.vertical, -3)
-                    .padding(.horizontal, 24)
-                    .onTapGesture {
-                        viewStore.send(.serviceAgreementToggled(viewStore.serviceAgreement))
-                    }
-                    
-                    HStack {
-                        Text("(필수) 개인정보 처리방침")
-                            .font(.pBody)
-                            .foregroundStyle(Color(.systemBlack))
-                        Spacer()
-                        Image(viewStore.privacyAgreement ? .checkmarkCircleFill : .checkmarkCircle )
-                    }
-                    .padding(.horizontal, 24)
-                    .onTapGesture {
-                        viewStore.send(.privacyAgreementToggled(viewStore.serviceAgreement))
-                    }
-                    .padding(.bottom, 106)
-                }
-                
-                if viewStore.isValid, viewStore.serviceAgreement, viewStore.privacyAgreement {
-                    Button {
-                        viewStore.send(.login(["exists_yn" : Const.isUser,
-                                               "access_token" : Const.accessToken,
-                                               "refresh_token" : Const.refreshToken,
-                                               "invite_cd" : Const.inviteCd,
-                                               "fm_dvcd" : Const.userType,
-                                               "nick_nm" : viewStore.nickname,
-                                               "email": Const.email,
-                                               "mem_sub": Const.memSub]))
-                    } label: {
-                        Text("가입하기")
-                            .font(.pButton2)
-                            .foregroundStyle(.white)
-                            .padding(.vertical, 16)
-                            .padding(.horizontal, 76)
-                            .background(Color(.mint50))
-                            .clipShape(RoundedRectangle(cornerRadius: 100))
+                    if viewStore.isValid, viewStore.serviceAgreement, viewStore.privacyAgreement {
+                        Button {
+                            viewStore.send(.login(["exists_yn" : Const.isUser,
+                                                   "access_token" : Const.accessToken,
+                                                   "refresh_token" : Const.refreshToken,
+                                                   "invite_cd" : Const.inviteCd,
+                                                   "fm_dvcd" : Const.userType,
+                                                   "nick_nm" : viewStore.nickname,
+                                                   "email": Const.email,
+                                                   "mem_sub": Const.memSub]))
+                        } label: {
+                            Text("가입하기")
+                                .font(.pButton2)
+                                .foregroundStyle(.white)
+                                .padding(.vertical, 16)
+                                .padding(.horizontal, 76)
+                                .background(Color(.mint50))
+                                .clipShape(RoundedRectangle(cornerRadius: 100))
+                        }
                     }
                 }
+                .padding(.horizontal, 30)
+                Spacer()
             }
-            .padding(.horizontal, 30)
-            Spacer()
+            .onTapGesture {
+                self.focusedField = false
+            }
         }
     }
 }
--- SsdamApp/Sources/Viewes/Tabs/Share/ShareView.swift
+++ SsdamApp/Sources/Viewes/Tabs/Share/ShareView.swift
@@ -18,16 +18,17 @@ struct ShareReducer: Reducer {
     @Dependency(\.mainQueue) var mainQueue
     
     let throttleId: String = "1"
-
+    
     struct State: Equatable {
         var code: String = ""
         var isValid: Bool = true
         var toastState: ToastReducer.State = .init()
         var NumberOfFamily: String = ""
         var joinPayload: FamilyJoinPayload = .init()
+        @BindingState var focusedField: Bool = false
     }
     
-    enum Action: Equatable {
+    enum Action: BindableAction, Equatable {
         case codeChanged(String)
         case codeValidation(String)
         case settingTapped
@@ -36,9 +37,11 @@ struct ShareReducer: Reducer {
         case fetchNumberOfFamilyResponse(TaskResult<NumberOfFamilyEntity>)
         case join(FamilyJoinBody)
         case joinReseponse(TaskResult<FamilyJoinEntity>)
+        case binding(BindingAction<State>)
     }
     
     var body: some ReducerOf<Self> {
+        BindingReducer()
         Scope(state: \.toastState, action: /Action.toast) {
             ToastReducer()
         }
@@ -102,6 +105,8 @@ struct ShareReducer: Reducer {
             case let .joinReseponse(.failure(error)):
                 print(error.localizedDescription)
                 return .none
+            case .binding:
+                return .none
             }
         }
     }
@@ -123,62 +128,71 @@ struct ShareReducer: Reducer {
 
 struct ShareView: View {
     let store: StoreOf<ShareReducer>
+    @FocusState var focusedField: Bool
     var body: some View {
         WithViewStore(self.store, observe: { $0 }) { viewStore in
-            VStack {
-                Text("\(Const.nickname.withAttributed(.pHeadline2))님 가족현황")
-                    .font(.pHeadline2)
-                    .padding(.bottom, 92)
-                
-                ZStack {
-                    Image(.ssmee)
-                        .offset(y: -60)
-                    VStack(spacing: 5) {
-                        Text("연결된 가족")
-                            .font(.pBody2)
-                        Text("\(viewStore.NumberOfFamily)명")
-                            .font(.pHeadline2)
+            ZStack {
+                Color.white
+                VStack {
+                    Text("\(Const.nickname.withAttributed(.pHeadline2))님 가족현황")
+                        .font(.pHeadline2)
+                        .padding(.bottom, 92)
+                    
+                    ZStack {
+                        Image(.ssmee)
+                            .offset(y: -60)
+                        VStack(spacing: 5) {
+                            Text("연결된 가족")
+                                .font(.pBody2)
+                            Text("\(viewStore.NumberOfFamily)명")
+                                .font(.pHeadline2)
+                        }
+                        .padding(.vertical, 36)
+                        .frame(maxWidth: .infinity)
+                        .background(Color(.gray10))
+                        .cornerRadius(10)
                     }
-                    .padding(.vertical, 36)
-                    .frame(maxWidth: .infinity)
-                    .background(Color(.gray10))
-                    .cornerRadius(10)
-                }
-                
-                ZStack(alignment: .trailing) {
-                    TextField("초대코드 입력하기", text: viewStore.binding(get: \.code, send: { value in
-                            .codeChanged(value)
-                    }))
-                    .font(.pButton)
-                    .foregroundStyle(Color(.systemBlack))
-                    .padding(.vertical, 16)
-                    .multilineTextAlignment(.center)
-                    .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color(.mint50), lineWidth: 2))
-                    .onChange(of: viewStore.code){ _, newValue in
-                        viewStore.send(.codeValidation(newValue))
-                        
+                    
+                    ZStack(alignment: .trailing) {
+                        TextField("초대코드 입력하기", text: viewStore.binding(get: \.code, send: { value in
+                                .codeChanged(value)
+                        }))
+                        .font(.pButton)
+                        .foregroundStyle(Color(.systemBlack))
+                        .padding(.vertical, 16)
+                        .multilineTextAlignment(.center)
+                        .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color(.mint50), lineWidth: 2))
+                        .focused($focusedField)
+                        .onChange(of: viewStore.code){ _, newValue in
+                            viewStore.send(.codeValidation(newValue))
+                            
+                        }
+                        .bind(viewStore.$focusedField, to: self.$focusedField)
+                        //                    if viewStore.isValid {
+                        //                        Image(.checkmarkCircleMint)
+                        //                            .padding(.vertical, 16)
+                        //                            .padding(.horizontal, 24)
+                        //                    }
                     }
-//                    if viewStore.isValid {
-//                        Image(.checkmarkCircleMint)
-//                            .padding(.vertical, 16)
-//                            .padding(.horizontal, 24)
-//                    }
+                    Text("*인증코드를 다시 확인해주세요")
+                        .font(.pCaption)
+                        .foregroundStyle(Color(.systemRed))
+                        .padding(.vertical, 12)
+                        .opacity(!viewStore.isValid ? 1 : 0)
+                    
+                    ShareLink(item: Const.inviteCd) {
+                        Text("내 초대코드 복사하기")
+                            .font(.pBody)
+                            .underline()
+                            .foregroundStyle(Color(.gray80))
+                    }
+                    .padding(.top, 21)
                 }
-                Text("*인증코드를 다시 확인해주세요")
-                    .font(.pCaption)
-                    .foregroundStyle(Color(.systemRed))
-                    .padding(.vertical, 12)
-                    .opacity(!viewStore.isValid ? 1 : 0)
-                
-                ShareLink(item: viewStore.code) {
-                    Text("내 초대코드 복사하기")
-                        .font(.pBody)
-                        .underline()
-                        .foregroundStyle(Color(.gray80))
-                }
-                .padding(.top, 21)
+                .padding(.horizontal, 30)
             }
-            .padding(.horizontal, 30)
+            .onTapGesture {
+                self.focusedField = false
+            }
             .toolbar(content: {
                 ToolbarItem(placement: .navigationBarLeading) {
                     Image(.logo)
